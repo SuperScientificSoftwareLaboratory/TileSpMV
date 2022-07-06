8a546d72a19f662773cb0b86b5ac556922e4562c
YuyaoNiu <76177643+YuyaoNiu@users.noreply.github.com>
2022-07-06 19:35:37 +0800

    src
    The updated src file has fixed some small bugs.

---
+++ src/external/CSR5_cuda/bench0.sh
@@ -0,0 +1,14 @@
+input="2757-matrix.csv"
+
+{
+  read
+  i=1
+  while IFS=',' read -r mid Group Name rows cols nonzeros
+  do
+    echo "$mid $Group $Name $rows $cols $nonzeros"
+    #./trsv_test ~/matrix-lu176/$name.l
+    #./sptrsv_d0 /home/weifeng/UFget/MM/$group/$name/$name.mtx
+    ./spmv_d0 /home/weifeng/UFget/MM/$Group/$Name/$Name.mtx
+    i=`expr $i + 1`
+  done 
+} < "$input"
